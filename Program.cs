98f58d0f78661714e9cf3c723c498b4b3dd20a78
Konstan668 <89112455963a@gmail.com>
2023-02-18 21:01:10 +0100

    Start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int[] GetSqrt(int num)
+{
+    int[] array = new int[num];
+    for (int i = 1; i <= num; i++)
+    {
+        array[i - 1] = i * i;
+    }
+    return array;
+}
+
+Console.WriteLine(string.Join(",", GetSqrt(2)));
